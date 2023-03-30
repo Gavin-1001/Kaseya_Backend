57063c2c7370acc3711abdf0f6d8e33e9c04de33
Gavin-1001 <gavinshelley10@gmail.com>
2023-03-30 12:13:59 +0100

    Added properties to JwtRefreshToken entity

--- src/main/java/com/example/backend/entity/JwtRefreshToken.java
+++ src/main/java/com/example/backend/entity/JwtRefreshToken.java
@@ -1,7 +1,25 @@
 package com.example.backend.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
 public class JwtRefreshToken {
+
+    @Id
+    @Column(name="token_id")
+    private String tokenId;
+
+    @Column(name="user_id")
+    private Long userId;
+
+    @Column(name="created_token_date")
+    private LocalDateTime whenTokenCreated;
+
+    @Column(name="expiration_date")
+    private LocalDateTime whenTokenExpirationDate;
+
+
 }
--- src/main/java/com/example/backend/service/JwtRefreshTokenServiceImpl.java
+++ src/main/java/com/example/backend/service/JwtRefreshTokenServiceImpl.java
@@ -1,7 +1,18 @@
 package com.example.backend.service;
 
+import com.example.backend.entity.JwtRefreshToken;
+import com.example.backend.entity.User;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JwtRefreshTokenServiceImpl extends JwtRefreshTokenService {
+public class JwtRefreshTokenServiceImpl implements JwtRefreshTokenService {
+    @Override
+    public JwtRefreshToken createRefreshToken(Long userId) {
+        return null;
+    }
+
+    @Override
+    public User generateAccessTokenFromRefreshToken(String refreshTokenId) {
+        return null;
+    }
 }
